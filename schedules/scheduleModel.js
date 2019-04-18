b24b96bcd0e23889810f558ecbcba62050e653db
Vladimir Turcan <Vladimir.Turcan@endava.com>
2019-04-18 11:14:59 +0300

    fixed the query issues

--- schedules/scheduleModel.js
+++ schedules/scheduleModel.js
@@ -1,4 +1,4 @@
-const db = require("../data/dbConfig");
+const db = require('../data/dbConfig')
 
 module.exports = {
   insert,
@@ -7,51 +7,48 @@ module.exports = {
   getAll,
   update,
   remove
-};
+}
+
+const joinQuery = 'schedules.id, schedules.pickup_name, schedules.pickup_date, schedules.pickup_time, schedules.pickup_comment, schedules.pickup_additional_comment, schedules.taken, schedules.volunteer_id, schedules.location_id, schedules.company_id, locations.office_name, locations.office_address, locations.office_email'.split(
+  ', '
+)
 
 function insert(location) {
-  return db("schedules")
+  return db('schedules')
     .insert(location)
-    .returning("id");
+    .returning('id')
 }
 
 function getOneById(id) {
-  return db
-    .select(
-      "schedules.id, schedules.pickup_name, schedules.pickup_date, schedules.pickup_time, schedules.pickup_comment, schedules.pickup_additional_comment, schedules.taken, schedules.volunteer_id, schedules.location_id, schedules.company_id, locations.office_name, locations.office_address, locations.office_email"
-    )
-    .from("schedules")
-    .where({ id })
-    .innerJoin("locations", "schedules.location_id", "=", "locations.id")
-    .first();
+  return db('schedules')
+    .innerJoin('locations', 'schedules.location_id', '=', 'locations.id')
+    .where({ 'schedules.id': id })
+    .select(joinQuery)
+    .first()
 }
 
 function getAllByCompanyId(id) {
   return db
-    .select(
-      "schedules.id, schedules.pickup_name, schedules.pickup_date, schedules.pickup_time, schedules.pickup_comment, schedules.pickup_additional_comment, schedules.taken, schedules.volunteer_id, schedules.location_id, schedules.company_id, locations.office_name, locations.office_address, locations.office_email"
-    )
-    .from("schedules")
+    .select(joinQuery)
+    .from('schedules')
     .where({ company_id: id })
-    .innerJoin("locations", "schedules.location_id", "=", "locations.id");
+    .innerJoin('locations', 'schedules.location_id', '=', 'locations.id')
 }
 
 function getAll() {
-  return db("schedules")
-    .innerJoin("locations", "schedules.location_id", "=", "locations.id")
-    .select(
-      "schedules.id, schedules.pickup_name, schedules.pickup_date, schedules.pickup_time, schedules.pickup_comment, schedules.pickup_additional_comment, schedules.taken, schedules.volunteer_id, schedules.location_id, schedules.company_id, locations.office_name, locations.office_address, locations.office_email"
-    );
+  return db('schedules')
+    .innerJoin('locations', 'schedules.location_id', '=', 'locations.id')
+    .select(joinQuery)
 }
 
 function update(id, fields) {
-  return db("schedules")
+  return db('schedules')
     .where({ id })
-    .update(fields);
+    .update(fields)
 }
 
 function remove(id) {
-  return db("schedules")
+  return db('schedules')
     .delete()
-    .where({ id });
+    .where({ id })
 }
--- schedules/schedulesRouter.js
+++ schedules/schedulesRouter.js
@@ -1,29 +1,29 @@
-const express = require("express");
-const Schedule = require("./scheduleModel");
-const authenticate = require("../middlewares/authMiddleware");
+const express = require('express')
+const Schedule = require('./scheduleModel')
+const authenticate = require('../middlewares/authMiddleware')
 
-const router = express.Router();
+const router = express.Router()
 
-router.use(authenticate);
+router.use(authenticate)
 
-router.get("/", async (req, res) => {
+router.get('/', async (req, res) => {
   try {
-    const schedules = await Schedule.getAll();
+    const schedules = await Schedule.getAll()
 
-    res.status(200).json(schedules);
+    res.status(200).json(schedules)
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json(err)
   }
-});
+})
 
-router.post("/", async (req, res) => {
+router.post('/', async (req, res) => {
   const {
     pickup_name,
     pickup_date,
     pickup_time,
     pickup_comment,
     location_id
-  } = req.body;
+  } = req.body
 
   if (
     !pickup_comment ||
@@ -32,7 +32,7 @@ router.post("/", async (req, res) => {
     !pickup_time ||
     !location_id
   ) {
-    return res.status(400).json({ msg: "Bad request" });
+    return res.status(400).json({ msg: 'Bad request' })
   }
 
   try {
@@ -40,85 +40,87 @@ router.post("/", async (req, res) => {
       ...req.body,
       taken: false,
       company_id: req.decoded.subject
-    });
+    })
 
-    res.status(201).json(newSchedule);
+    res.status(201).json(newSchedule)
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json(err)
   }
-});
+})
 
-router.patch("/:id", async (req, res) => {
-  const { pickup_name, pickup_date, pickup_time, pickup_comment } = req.body;
+router.patch('/:id', async (req, res) => {
+  const { pickup_name, pickup_date, pickup_time, pickup_comment } = req.body
 
   if (!pickup_comment || !pickup_date || !pickup_name || !pickup_time) {
-    return res.status(400).json({ msg: "Bad request" });
+    return res.status(400).json({ msg: 'Bad request' })
   }
 
   try {
-    const oldSchedule = await Schedule.getOneById(req.params.id);
+    const oldSchedule = await Schedule.getOneById(req.params.id)
+
+    console.log(oldSchedule)
 
     if (!oldSchedule) {
-      return res.status(404).json({ msg: "This schedule does not exist" });
+      return res.status(404).json({ msg: 'This schedule does not exist' })
     }
 
     if (oldSchedule.company_id !== req.decoded.subject) {
-      return res.status(401).json({ msg: "Unauthorized" });
+      return res.status(401).json({ msg: 'Unauthorized' })
     }
 
     const newSchedule = await Schedule.update(req.params.id, {
       ...req.body
-    });
+    })
 
-    res.status(201).json(newSchedule);
+    res.status(201).json(newSchedule)
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json(err)
   }
-});
+})
 
-router.patch("/:id/take", async (req, res) => {
+router.patch('/:id/take', async (req, res) => {
   try {
-    const schedule = await Schedule.getOneById(req.params.id);
+    const schedule = await Schedule.getOneById(req.params.id)
 
     if (!schedule) {
-      return res.status(404).json({ msg: "Schedule not found" });
+      return res.status(404).json({ msg: 'Schedule not found' })
     }
 
     if (schedule.company_id === req.decoded.subject) {
       res
         .status(422)
-        .json({ msg: "A company can not accept a request for volunteers" });
+        .json({ msg: 'A company can not accept a request for volunteers' })
     }
 
     const newSchedule = await Schedule.update(req.params.id, {
       volunteer_id: req.decoded.subject,
       taken: true
-    });
+    })
 
-    res.status(201).json(newSchedule);
+    res.status(201).json(newSchedule)
   } catch (error) {
-    res.status(500).json(error);
+    res.status(500).json(error)
   }
-});
+})
 
-router.delete("/:id", async (req, res) => {
-  const oldSchedule = await Schedule.getOneById(req.params.id);
+router.delete('/:id', async (req, res) => {
+  const oldSchedule = await Schedule.getOneById(req.params.id)
 
   if (!oldSchedule) {
-    return res.status(404).json({ msg: "This schedule does not exist" });
+    return res.status(404).json({ msg: 'This schedule does not exist' })
   }
 
   if (oldSchedule.company_id !== req.decoded.subject) {
-    return res.status(401).json({ msg: "Unauthorized" });
+    return res.status(401).json({ msg: 'Unauthorized' })
   }
 
   try {
-    const deleted = await Schedule.remove(req.params.id);
+    const deleted = await Schedule.remove(req.params.id)
 
-    return res.status(200).json(deleted);
+    return res.status(200).json(deleted)
   } catch (error) {
-    res.status(500).json(error);
+    res.status(500).json(error)
   }
-});
+})
 
-module.exports = router;
+module.exports = router
